ef68d1cbcb29f214ef6b90c68dbc46c7f2f2d719
Andrew Hilton <hiltoa02@MC-S001890.local>
2010-03-23 15:25:31 +0000

    fixing the test's so glow only run's if it's on bbc.co.uk

--- bbc.js
+++ bbc.js
@@ -35,8 +35,8 @@ function getNextData(key,outlet){
 }
 
 function makeAjaxCall(feed_url) { 
-    if(typeof (window.glow) == 'object')      { return makeAjaxCall_glow(feed_url);   }
-    if(typeof (window.jQuery) == 'function')  { return makeAjaxCall_jquery(feed_url); }
+    if(typeof (window.glow) == 'object' && location.host == "www.bbc.co.uk")      { return makeAjaxCall_glow(feed_url);   }
+    if(typeof (window.jQuery) == 'function')                                      { return makeAjaxCall_jquery(feed_url); }
     
 }
 
